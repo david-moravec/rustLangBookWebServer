60871bd896884401df474d7659f02d05718873f1
David Moravec <moravec@numsolution.cz>
2023-10-04 14:35:33 +0200

    Show simple text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
@@ -17,10 +18,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;1024];
 
     stream.read(&mut buffer).unwrap();
-    println!(
-        "Request: {}",
-        String::from_utf8_lossy(&buffer[..])
+    
+    let contents = fs::read_to_string("index.html").unwrap();
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
     );
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
